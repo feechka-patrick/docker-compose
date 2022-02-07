653cc37129d671fbddcb35a5f6eb02ea48d75a1f
feechka-patrick <annetatimoshenko38@gmail.com>
2022-02-07 19:45:07 +0300

    delete all

--- fclean.sh
+++
@@ -1,6 +0,0 @@
-docker-compose -f srcs/docker-compose.yml down
-docker rmi -f $(docker images -qa)
-docker volume rm $(docker volume ls -q)
-docker system prune -a -f
-sudo rm -rf /home/nmisfit/data/wordpress/*
-sudo rm -rf /home/nmisfit/data/mariadb/*
